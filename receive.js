c2236df32be208204e7dc99fe6ab754b1bb9ae37
张若瑾 <ruojin@zhangruojindeMacBook-Pro.local>
2019-03-06 18:46:21 -0500

    try

--- receive.js
+++ receive.js
@@ -4,13 +4,12 @@ var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var qu = 'hello';
     var ex = 'ex';
 
     ch.assertExchange(ex, 'direct', {durable:false})
-    ch.assertQueue(qu, function (err,q){
+    ch.assertQueue(' ', function (err,q){
       console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-      ch.bindQueue(q.queue, ex, qu);
+      ch.bindQueue(q.queue, ex, ' ');
 
       ch.consume(q, function(msg) {
         console.log(" [x] Received %s", msg.content.toString());
