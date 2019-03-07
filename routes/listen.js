d852581cbfd615da914e21494d20db82f8d58da5
张若瑾 <ruojin@zhangrujindeMBP.home>
2019-03-06 22:38:00 -0500

    123

--- routes/listen.js
+++ routes/listen.js
@@ -3,6 +3,10 @@ var bodyParser = require('body-parser');
 var router = express.Router();
 const path = require('path');
 
+router.post('/',function(req,res){
+    res.send("OK")
+});
+
 
 
 module.exports = app;
