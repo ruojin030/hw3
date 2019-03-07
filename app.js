55f28d3c1aceab318172825ed8b5c55b6cba7932
张若瑾 <ruojin@zhangrujindeMBP.home>
2019-03-07 01:33:34 -0500

    may success?

--- app.js
+++ app.js
@@ -1,19 +1,62 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var amqp = require('amqplib/callback_api');
+var longpoll = require("express-longpoll")
 var port = 80
 
 
 var app = express()
-var listens = require('./routes/listen')
+
+amqp = require('amqplib/callback_api')
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.post('/listen',function(req, res){
+    var key = req.body.key
+    var ex = 'hw3'
+    amqp.connect('amqp://localhost', function(err, conn) {
+        conn.createChannel(function(err, ch){
+            ch.assertExchange(ex, 'direct', {durable:false})
+            console.log("Waiting for logs")
+            ch.assertQueue('',{exclusive:true},function(err,q){
+                for(x in key){
+                    ch.bindQueue(q.queue, ex, x);
+                    console.log('key is '+x)
+                }
+                ch.consume(q.queue, function(msg){
+                    console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
+                    res.send({'msg':msg})
+                },{noAck:true})
+            });
+        });    
+   });
+});
 
-app.use('/listen', listens)
-app.listen(port)
+app.post('/speak',function(req,res){
+    var key = req.body.key
+    var msg = req.body.msg
+    amqp.connect('amqp://localhost', function(err, conn) {
+    conn.createChannel(function(err, ch) {
+    var ex = 'hw3';
+    var m = msg
+    var severity = (args.length > 0) ? args[0] : 'info';
+
+    ch.assertExchange(ex, 'direct', {durable: false});
+    ch.publish(ex, key, new Buffer(m));
+    console.log(" [x] Sent %s: '%s'", key, m);
+  });
+
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
+    
+
+
+})
+
 
 module.exports = app;
