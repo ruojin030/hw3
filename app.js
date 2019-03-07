a536c5b80e1fedd023d061c1c67f1e29a9f52865
张若瑾 <ruojin@zhangrujindeMBP.home>
2019-03-06 22:26:06 -0500

    123

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express()
+var listen = require('./routes/listen')
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/listen', listen)
+
+
+module.exports = app;
